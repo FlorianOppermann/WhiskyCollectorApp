354987363fd541ac6f4463ef2c4f8e4d1229c39d
FlorianOppermann <florianoppermann01@gmail.com>
2024-10-07 14:28:36 +0200

    made simple test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,46 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:test_application/main.dart';
+import 'package:whisky_collector/data/whisky.dart';
+import 'package:whisky_collector/profile_screen.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('DashboardPage displays correct statistics',
+      (WidgetTester tester) async {
+    // Arrange
+    /* Whisky({
+    required this.distillery,
+    required this.age,
+    required this.price,
+    this.imagePath,
+    this.type,
+    this.aroma,
+    this.taste,
+    this.finish,
+    this.flavorProfiles = const [],
+    this.alcoholContent,
+    this.region,
+    this.rating,
+    this.notes = const [], // Initialisierung der Liste
+  });*/
+    final whiskies = [
+      Whisky(distillery: 'Glenfiddich', age: 12, price: 50.0),
+      Whisky(distillery: 'Macallan', age: 15, price: 70.0),
+    ];
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Act
+    await tester.pumpWidget(MaterialApp(
+      home: ProfileScreen(whiskies: whiskies),
+    ));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Assert
+    expect(find.text('Anzahl Whiskies'), findsOneWidget);
+    expect(find.text('2'), findsOneWidget);
+    expect(find.text('Durchschnittsalter'), findsOneWidget);
+    expect(find.text('13.5Jahre'), findsOneWidget);
+    expect(find.text('Durchschnittspreis'), findsOneWidget);
+    expect(find.text('60.00€'), findsOneWidget);
+    expect(find.text('Gesamtwert'), findsOneWidget);
+    expect(find.text('120.00€'), findsOneWidget);
+    expect(find.text('Durchschnitts- bewertung'), findsOneWidget);
   });
 }
