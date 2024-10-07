2eb1adb730ea0ceb9cebb76ec39b46c593b01835
FlorianOppermann <florianoppermann01@gmail.com>
2024-10-07 14:09:36 +0200

    Createt simple but stylish profile

---
+++ lib/lib/widgets/stat_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class StatCard extends StatelessWidget {
+  final String title;
+  final String value;
+
+  const StatCard({
+    Key? key,
+    required this.title,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              value,
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
